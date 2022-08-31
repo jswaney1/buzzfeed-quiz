0a912d3c70983049646798f7ddf20f9a81970b61
Jenna Swaney <jennalswaney@gmail.com>
2022-08-30 19:30:28 -0500

    Working-on-it

--- submit.js
+++ submit.js
@@ -3,13 +3,12 @@ function Submit()
     console.log("hello");
     let inputElement= document.getElementById("user-feedback");
     console.log(inputElement);
-    alert(inputElement.value);
-    if(inputElement.value>20)
+    if(inputElement.value.length>20)
     {
         alert("Thank you for submitting your feedback!");
     }
     else 
     {
-        alert("Please give more detail.");
+        alert("Please give more detail so we know exactly what you want to see in your next quiz :)");
     }
 }
